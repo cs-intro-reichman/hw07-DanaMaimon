0fa65f381e4b452a645797616011f8bb33ec967a
Dana Maimon <danamaimon@MacBook-Air-sl-Dana.local>
2026-01-17 00:05:12 +0200

    firstTry

--- IsSorted.java
+++ IsSorted.java
@@ -5,28 +5,21 @@ public class IsSorted {
         return isSorted(a, 0);
     }
 
-    // Helper recursive function
     private static boolean isSorted(int[] a, int i) {
-        // Helper recursive function
-        // 1. תנאי עצירה: הגענו לסוף המערך (או שהמערך ריק), הכל תקין
         if (i >= a.length - 1) {
             return true;
         }
-
-        // 2. בדיקה: אם האיבר הנוכחי גדול מהבא אחריו - הסדר שגוי
         if (a[i] > a[i + 1]) {
             return false;
         }
-
-        // 3. צעד רקורסיבי: בדוק את הזוג הבא
         return isSorted(a, i + 1);
     }
 
     public static void main(String[] args) {
         int[] a = {1, 2, 2, 5};
-        System.out.println(isSorted(a)); // true
+        System.out.println(isSorted(a));
 
         int[] b = {1, 3, 2};
-        System.out.println(isSorted(b)); // false
+        System.out.println(isSorted(b)); 
     }
 }
